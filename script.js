a30cd2ba584428b2e105b6294bf01addeb43212f
ARna06 <72038543+Leopard005537@users.noreply.github.com>
2024-02-15 14:34:19 +0530

    Add every functionality except sign change

--- script.js
+++ script.js
@@ -12,129 +12,185 @@ const nine = document.querySelector('.nine');
 const operationZone = document.querySelector('.operationZone');
 const buttons = document.querySelector('.button-container');
 
-let number = '0';
-operationZone.textContent = number;
+let evaluator = '0';
+let running = evaluator;
+
+operationZone.textContent = running;
 
 zero.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else { number = number + '0';}
-    operationZone.textContent = number;
+    if (running.length > 14) {
+        running = running;
+    }else { running = running + '0';}
+    operationZone.textContent = running;
+    evaluator = evaluator + '0';
 });
 one.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    } else {number = number + '1';}
-    operationZone.textContent = number;
+    if (running.length > 14) {
+        running = running;
+    } else {running = running + '1';}
+    operationZone.textContent = running;
+    evaluator = evaluator + '1';
 });
 two.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    } else{number = number + '2';};
+    if (running.length > 14) {
+        running = running;
+    } else{running = running + '2';};
 
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '2';
 });
 three.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '3';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '3';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '3';
 });
 four.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '4';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '4';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '4';
 });
 five.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '5';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '5';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '5';
 });
 six.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '6';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '6';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '6';
 });
 seven.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '7';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '7';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '7';
 });
 eight.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '8';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '8';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '8';
 });
 nine.addEventListener('click', (e) => {
-    if (number[0] == '0') {
-        number = number.slice(1);
+    if ((running[0] == '0') && (running.length === 1)) {
+        running = running.slice(1);
     }
-    if (number.length > 9) {
-        number = number;
-    }else{number = number + '9';}
+    if (running.length > 14) {
+        running = running;
+    }else{running = running + '9';}
     
-    operationZone.textContent = number;
+    operationZone.textContent = running;
+    evaluator = evaluator + '9';
 });
 
 const clear = document.querySelector('.clear');
 clear.addEventListener('click' , (e) => {
-    number = '0';
-    operationZone.textContent = number;
+    running = '0';
+    evaluator = '0';
+    operationZone.textContent = running;
 })
-
-let number1 = '0';
-let number2 = '0';
-result = '0';
-
-const product = document.querySelector('.multiplication');
-product.addEventListener('click', () => {
-    number1 = number;
-    number = '0';
-    operationZone.textContent = number;
-    number2 = number1;
-    number = '0';
-    result = (Number(number1)*Number(number2)).toString();
-});
 
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', (e) =>{
-    operationZone.textContent = result;
+    while (evaluator.charAt(0) === '0') {
+        evaluator = evaluator.substring(1);
+    }
+    running = eval(evaluator).toString();
+    if (running.length > 15) {
+        running = running.substring(0, 14);
+    }
+    evaluator = running;
+    operationZone.textContent = running;
+});
+
+const sum = document.querySelector('.plus');
+sum.addEventListener('click', () => {
+    evaluator = evaluator + ' + ';
+    running = '0';
+    operationZone.textContent = running;
+});
+
+const product = document.querySelector('.multiplication');
+product.addEventListener('click', () => {
+    evaluator = evaluator + ' * ';
+    running = '0';
+    operationZone.textContent = running;
+});
+
+const division = document.querySelector('.division');
+division.addEventListener('click', () => {
+    evaluator = evaluator + ' / ';
+    running = '0';
+    operationZone.textContent = running;
+});
+
+const difference= document.querySelector('.minus');
+difference.addEventListener('click', () => {
+    evaluator = evaluator + ' - ';
+    running = '0';
+    operationZone.textContent = running;
+});
+
+const decimal = document.querySelector('.decimal');
+decimal.addEventListener('click', () => {
+    if (!running.includes('.')){
+        running = running + '.';
+        evaluator = evaluator + '.';
+        operationZone.textContent = running;
+    }
+});
+
+const percentage = document.querySelector('.percent');
+percentage.addEventListener('click', () => {
+    evaluator = evaluator + ' / 100 * ';
+    running = '0';
+    operationZone.textContent = running;
+})
+
+const signChange = document.querySelector('.sign');
+signChange.addEventListener('click', () => {
+    alert("It hasn't been implemented yet");
 });
